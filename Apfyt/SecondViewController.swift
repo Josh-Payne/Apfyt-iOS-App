806c2b42971f78658fcd07f2738d347d689f3331
Josh Payne <joshpayne1@rescomp-16-335460.stanford.edu>
2017-02-19 21:22:55 -0800

    Initial Commit

---
+++ Apfyt/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Apfyt
+//
+//  Created by Josh Payne on 2/19/17.
+//  Copyright © 2017 Apfyt. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
